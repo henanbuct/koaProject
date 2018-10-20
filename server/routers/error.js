761348996fd98e2ef71545dce7aeaf578c318e80
henanbuct <13430940086@163.com>
2018-10-20 15:30:49 +0800

    add server

---
+++ server/routers/error.js
@@ -0,0 +1,10 @@
+/**
+ * 错误页面子路由
+ */
+const router = require('koa-router')()
+module.exports = router.get('*',  async ( ctx ) => {
+  const title = 'error'
+  await ctx.render('error', {
+    title
+  })
+})
